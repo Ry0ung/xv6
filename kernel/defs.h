0e4d55fb345a8a467febf1144497e79bb41538e0
ryu_u <603131011@qq.com>
2025-06-18 08:57:49 +0800

    lab6-all

--- kernel/defs.h
+++ kernel/defs.h
@@ -64,6 +64,11 @@ void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
 
+int             cowpage(pagetable_t, uint64);
+void*           cowalloc(pagetable_t, uint64);
+int             krefcnt(void*);
+int             kaddrefcnt(void*);
+
 // log.c
 void            initlog(int, struct superblock*);
 void            log_write(struct buf*);
@@ -168,6 +173,7 @@ void            uvmfree(pagetable_t, uint64);
 void            uvmunmap(pagetable_t, uint64, uint64, int);
 void            uvmclear(pagetable_t, uint64);
 uint64          walkaddr(pagetable_t, uint64);
+pte_t *         walk(pagetable_t, uint64, int);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
